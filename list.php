11da1623d8ae40dc435c3a3098342beb1ffb2a59
James Nylen <jnylen@gmail.com>
2014-09-16 07:33:27 -0500

    Remove duplicate doctype (oops)

--- list.php
+++ list.php
@@ -3,7 +3,6 @@ require_once 'common.php';
 read_data();
 ?>
 <!DOCTYPE html>
-<!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="lt-ie9"> <![endif]-->
