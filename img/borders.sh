c0f59d540a269d5ee6400087baf75251d59e5d79
James Nylen <jnylen@gmail.com>
2016-08-09 04:57:13 +0000

    Update image borders to match GitHub page design

--- img/borders.sh
+++ img/borders.sh
@@ -6,9 +6,7 @@ for i in *-orig.png; do
     out="${i%-orig.png}.png"
 
     convert "$i" \
-        -bordercolor '#cacaca' \
+        -bordercolor '#dddddd' \
         -border 1x1 \
-        -bordercolor '#eeeeee' \
-        -border 2x2 \
         "$out"
 done
